5d48b0f0d0e6609f6b36ce4944b10dd232b195ce
Manuel Duarte <carlosd99m@gmail.com>
2021-11-30 02:19:01 -0600

    refactored to clickable buttons

--- XFProfile/MainPage.xaml.cs
+++ XFProfile/MainPage.xaml.cs
@@ -12,12 +12,12 @@ namespace XFProfile
 
         private async void PhoneClicked(object sender, EventArgs e)
         {
-            await DisplayAlert("Contact", "Phone clicked!", "OK");
+            await DisplayAlert("Twitter", "Twitter clicked!", "OK");
         }
 
         private async void MailClicked(object sender, EventArgs e)
         {
-            await DisplayAlert("Contact", "Mail clicked!", "OK");
+            await DisplayAlert("Instagram", "Instagram clicked!", "OK");
         }
 
         private async void FollowClicked(object sender, EventArgs e)
