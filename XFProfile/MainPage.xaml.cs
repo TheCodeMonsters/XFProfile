7c67e5db70328eba5fd727f2ea28faee5004cc51
Manuel Duarte <carlosd99m@gmail.com>
2021-11-30 01:24:31 -0600

    feat: project setup

---
+++ XFProfile/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XFProfile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void PhoneClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Contact", "Phone clicked!", "OK");
+        }
+
+        private async void MailClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Contact", "Mail clicked!", "OK");
+        }
+
+        private async void FollowClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("ShowMore", "Show More clicked!", "OK");
+        }
+    }
+}
